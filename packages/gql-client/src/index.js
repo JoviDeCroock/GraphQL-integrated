b29348ea2ee1400560002597b020e518adc78c5f
jdecroock <decroockjovi@gmail.com>
2019-04-14 13:48:11 +0200

    client

---
+++ packages/gql-client/src/index.js
@@ -0,0 +1,12 @@
+(function() {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('gql-client.js').then(() => {
+      console.info('Initialized');
+    }).catch(function(err) {
+      console.warn('Failed');
+      console.error(err);
+    });
+  } else {
+    console.warn('No sw implementation');
+  }
+}());
